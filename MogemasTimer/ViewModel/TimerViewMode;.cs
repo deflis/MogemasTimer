03ae6f92fbe33960a6e32b82da302488ca58062d
deflis <deflis@gmail.com>
2012-04-17 04:07:44 +0900

    init commit

---
+++ MogemasTimer/ViewModel/TimerViewMode;.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+
+namespace MogemasTimer.ViewModel
+{
+    class Timer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
